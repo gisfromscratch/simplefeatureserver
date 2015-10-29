e56ef9b8b0cccec912038d59c26c5e13df6273f3
J.T <gisfromscratch@live.de>
2015-10-29 22:54:31 +0100

    Starting to set all properties for a feature layer

--- WCF Simple Feature Server/Simple Feature Service/Simple Feature Datasource/Data/FeatureLayer.cs
+++ WCF Simple Feature Server/Simple Feature Service/Simple Feature Datasource/Data/FeatureLayer.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using GIS.Datasources.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,11 +30,125 @@ namespace GIS.Datasources.Data
     [DataContract]
     public class FeatureLayer
     {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        public FeatureLayer()
+        {
+            Id = 0;
+            Name = string.Empty;
+            DisplayField = string.Empty;
+            Description = string.Empty;
+            Copyright = string.Empty;
+            IsDefaultVisible = true;
+            IsDataVersioned = false;
+            SupportsRollbackOnFailure = false;
+            SupportsStatistics = false;
+            SupportsAdvancedQueries = false;
+            GeometryType = @"esriGeometryPoint";
+            MinScale = 0.0;
+            MaxScale = 0.0;
+            EffectiveMinScale = 0.0;
+            EffectiveMaxScale = 0.0;
+            HasM = false;
+            HasZ = false;
+            EnableZDefaults = false;
+            ZDefault = 0.0;
+            AllowGeometryUpdates = false;
+            HasAttachments = false;
+            ObjectIdFieldName = string.Empty;
+            GlobalIdFieldName = string.Empty;
+            TypeIdFieldName = string.Empty;
+        }
+
         [DataMember(Name = @"id")]
         public int Id { get; set; }
 
         [DataMember(Name = @"name")]
         public string Name { get; set; }
+
+        [DataMember(Name = @"type")]
+        public string Type
+        {
+            get
+            {
+                return @"Feature Layer";
+            }
+
+            set
+            {
+                // Ignore any values
+            }
+        }
+
+        [DataMember(Name = @"displayField")]
+        public string DisplayField { get; set; }
+
+        [DataMember(Name = @"description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = @"copyrightText")]
+        public string Copyright { get; set; }
+
+        [DataMember(Name = @"defaultVisibility")]
+        public bool IsDefaultVisible { get; set; }
+
+        [DataMember(Name = @"isDataVersioned")]
+        public bool IsDataVersioned { get; set; }
+
+        [DataMember(Name = @"supportsRollbackOnFailureParameter")]
+        public bool SupportsRollbackOnFailure { get; set; }
+
+        [DataMember(Name = @"supportsStatistics")]
+        public bool SupportsStatistics { get; set; }
+
+        [DataMember(Name = @"supportsAdvancedQueries")]
+        public bool SupportsAdvancedQueries { get; set; }
+
+        [DataMember(Name = @"geometryType")]
+        public string GeometryType { get; set; }
+
+        [DataMember(Name = @"minScale")]
+        public double MinScale { get; set; }
+
+        [DataMember(Name = @"maxScale")]
+        public double MaxScale { get; set; }
+
+        [DataMember(Name = @"effectiveMinScale")]
+        public double EffectiveMinScale { get; set; }
+
+        [DataMember(Name = @"effectiveMaxScale")]
+        public double EffectiveMaxScale { get; set; }
+
+        [DataMember(Name = @"extent")]
+        public Envelope Extent { get; set; }
+
+        [DataMember(Name = @"hasM")]
+        public bool HasM { get; set; }
+
+        [DataMember(Name = @"hasZ")]
+        public bool HasZ { get; set; }
+
+        [DataMember(Name = @"enableZDefaults")]
+        public bool EnableZDefaults { get; set; }
+
+        [DataMember(Name = @"zDefault")]
+        public double ZDefault { get; set; }
+
+        [DataMember(Name = @"allowGeometryUpdates")]
+        public bool AllowGeometryUpdates { get; set; }
+
+        [DataMember(Name = @"HasAttachments")]
+        public bool HasAttachments { get; set; }
+
+        [DataMember(Name = @"objectIdField")]
+        public string ObjectIdFieldName { get; set; }
+
+        [DataMember(Name = @"globalIdField")]
+        public string GlobalIdFieldName { get; set; }
+
+        [DataMember(Name = @"typeIdField")]
+        public string TypeIdFieldName { get; set; }
 
         /// <summary>
         /// The connection string referencing the underlying datasource.
